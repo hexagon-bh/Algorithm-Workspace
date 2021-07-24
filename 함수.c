3a3245f44f7d135414c247a7a2a62fdf41ef23c6
hyun0810d <hyun0810d@gmail.com>
2021-07-24 23:16:01 +0900

    commit

---
+++ 함수.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+//(리턴타입)(함수이름)(인자/variable)
+void star()
+{
+    printf("**************************\n");
+}
+
+int main()
+{
+    printf("안녕....\n");
+    star();
+    printf("반가워......\n");
+    star();
+}
