c7fc68f9d00bdd91112414b32c6daa63a42da3d9
hyun0810d <hyun0810d@gmail.com>
2021-07-24 19:45:40 +0900

    complete

--- 선택정렬.c
+++ 선택정렬.c
@@ -2,20 +2,31 @@
 int main()
 {
 	int a[10]={3,1,5,6,7,2,8,4,9,0};
-	int min=3;
-	int b=0;
-	for(int j=0;j<=9;j++)
+	int min=0;
+	int b,n,t,i=0;
+	
+	for(int j=0;j<=8;j++)
 	{
-		for(int i=b;i<=9;i++)
+		min=a[j];
+		for(i=j+1;i<=9;i++)
 		{
-			if(min>a[i+1])
+			if(min>a[i])
 			{
-				min=a[i+1];
+				min=a[i];
 			}
 		}
-		a[j]=min;
-		min=a[j];
-		b++;
+		
+		for(int z=0;z<=9;z++)
+		{
+			if(a[z]==min)
+			{
+				t=a[j];
+				a[j]=min;
+				a[z]=t;
+			}
+		}
+		
+		
 	}
 	
 	for(int c=0;c<=9;c++)
