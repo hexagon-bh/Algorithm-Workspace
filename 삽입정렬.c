3aa93aa0f618d92b804847151b75dee2a26c396d
hyun0810d <hyun0810d@gmail.com>
2021-08-17 17:05:35 +0900

    complete-삽입정렬.c

--- 삽입정렬.c
+++ 삽입정렬.c
@@ -1,26 +1,19 @@
 #include <stdio.h>
 int main()
 {
-    int a[8]={5,8,1,6,2,4,9,3,7};
+    int a[9]={5,8,1,6,2,4,9,3,7};
     int number=1;
     int t_number=1;
     int t=0;
 
     for(int i=0;i<=7;i++)
     {
-        while(1)
+        while(a[number]>a[number-1])
         {
-            if(a[number-1]<a[number])
-            {
-                continue;
-            }
-            else
-            {
                t=a[number-1];
                a[number-1]=a[number];
                a[number]=t;
-               number--;
-            }    
+               number--;  
         }
         t_number++;
         number=t_number;
@@ -28,6 +21,6 @@ int main()
 
     for(int j=0;j<=8;j++)
     {
-        printf("%d",a[j]);
+        printf("%3d",a[j]);
     }
 }
