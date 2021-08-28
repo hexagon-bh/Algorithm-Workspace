f65a59c693c53d7ee604dcd5b3507c45f5855459
hyun0810d <84117112+hyun0810d@users.noreply.github.com>
2021-08-28 15:00:38 +0900

    Create jungol.c

---
+++ jungol.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+int main()
+{
+//	printf("TTTTTTTTTT\nTTTTTTTTTT\n    TT    \n    TT    \n    TT    ");
+
+//	int Kor=90;
+//	int mat=80;
+//	int eng=100;
+//	int sum,avg=0;
+//	printf("%d\n",Kor);
+//	printf("%d\n",mat);
+//	printf("%d\n",eng);
+//	sum=Kor+mat+eng;
+//	printf("%d\n",sum);
+//	avg=sum/3;
+//	printf("%d\n",avg);
+
+//int a,b,c=0;
+//scanf("%d",&a);
+//scanf("%d",&b);
+//scanf("%d",&c);
+//printf("%d+%d=%d",a,b,c);
+
+//float a,b,c=0;
+//scanf("%f",&a);
+//scanf("%f",&b);
+//scanf("%f",&c);
+//printf("%.2f+%.2f=%.2f",a,b,c);
+
+float a=0,b=0;
+scanf("%f",&a);
+scanf("%f",&b);
+printf("%.2f*%.0f=%.0f",a,b,a*b);
+
+}
