7693a9eaf33117b90986a30b3429d9969dc70f0e
hyun0810d <hyun0810d@gmail.com>
2021-08-30 23:42:35 +0900

    modify

---
+++ jungol/입력-형성평가2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    float a,b,c=0;
+    scanf("%f",&a);
+    scanf("%f",&b);
+    scanf("%f",&c);
+    printf("%.2f+%.2f=%.2f",a,b,c);
+}
---
+++ jungol/입력-형성평가3.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+    float a=0,b=0;
+    scanf("%f",&a);
+    scanf("%f",&b);
+    printf("%.2f*%.0f=%.0f",a,b,a*b);
+}
