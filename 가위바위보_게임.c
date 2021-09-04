0f7ead4a572689eedb46eadcfb56aaaafadada9d
hyun0810d <hyun0810d@gmail.com>
2021-09-04 10:30:01 +0900

    switch~scase

--- 가위바위보_게임.c
+++ 가위바위보_게임.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
-int mian()
+int main()
 {
-    printf ("hello_world");
-    return 0;
+    int mine=0;
+    char mine_c[50];
+    int computer=0;
+
+    printf("----------가위 바위 보_게임----------\n");
+    printf("MY: ");
+    scanf("%s",&mine_c);
+    if(mine>2)
+    {
+        return 0;
+    }
+    srand((unsigned) time(NULL));
+    computer=rand()%3;
+    switch(computer)
+    {
+        case 0:
+            printf("Computer: 가위");
+            break;
+        case 1:
+            printf("Computer: 바위");
+            break;
+        case 2:
+            printf("Computer: 보");
+            break;
+    }
+
 }
