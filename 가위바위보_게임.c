24b8da85189c4eb4cf2360144f35965c9d10fcbb
hyun0810d <hyun0810d@gmail.com>
2021-09-04 11:08:51 +0900

    mine_c의 경우에따라 mine의값을 정한다

--- 가위바위보_게임.c
+++ 가위바위보_게임.c
@@ -1,30 +1,52 @@
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
 int main()
 {
-    int mine=0;
-    char mine_c[50];
-    int computer=0;
+        int mine=0;
+        char mine_c[50];
+        int computer=0;
+        int  tie,win,lose=0;
 
-    printf("----------가위 바위 보_게임----------\n");
-    printf("MY: ");
-    scanf("%s",&mine_c);
-    if(mine>2)
-    {
-        return 0;
-    }
-    srand((unsigned) time(NULL));
-    computer=rand()%3;
-    switch(computer)
-    {
-        case 0:
-            printf("Computer: 가위");
-            break;
-        case 1:
-            printf("Computer: 바위");
-            break;
-        case 2:
-            printf("Computer: 보");
-            break;
-    }
+        while(1)
+        {
+        printf("----------가위 바위 보_게임----------\n");
 
+        printf("MY: ");
+        scanf("%s",&mine_c);
+        if(mine_c=="가위")
+        {
+            mine=1;
+        }
+        if(mine_c=="바위")
+        {
+            mine=2;
+        }
+        if(mine_c=="보")
+        {
+            mine=3;
+        }
+        if(mine>2)
+        {
+            system("cls");
+            continue;
+        }
+
+        srand((unsigned) time(NULL));
+        computer=rand()%3;
+        switch(computer)
+        {
+            case 0:
+                printf("Computer: 가위");
+                break;
+            case 1:
+                printf("Computer: 바위");
+                break;
+            case 2:
+                printf("Computer: 보");
+                break;
+        }
+        
+        system("cls"); //reset screen
+    }
 }
