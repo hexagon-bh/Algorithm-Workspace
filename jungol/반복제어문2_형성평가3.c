10f6e534ef4fde9bc0a11a8841c9c935c2b040ca
hyun0810d <hyun0810d@gmail.com>
2021-10-04 23:32:31 +0900

    save

---
+++ jungol/반복제어문2_형성평가3.c
@@ -0,0 +1,18 @@
+//문제 - 정수를 입력받아서 1부터 입력받은 정수까지의 5의 배수의 합을 구하여 출력하는 프로그램을 작성하시오.
+
+#include <stdio.h>
+int main()
+{
+    int input,value=0;
+    int a=1;
+    scanf("%d",&input);
+    while(a<=input)
+    {
+        if(a%5==0)
+        {
+            value=value+a;
+        }
+    }
+    printf("%d",value);
+    return 0;
+}
