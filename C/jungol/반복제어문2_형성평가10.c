6a6023f758c32fecd0f7464af94bb31365149ac9
hyun0810d <hyun0810d@gmail.com>
2021-10-09 21:18:18 +0900

    반복제어문2_형성평가10.c

---
+++ C/jungol/반복제어문2_형성평가10.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int multiplication_table(int number)
+{
+    for(int i=1;i<=9;i++)
+    {
+        printf("%d * %d = %d\n",number,i,number*i);
+    }
+}
+int main()
+{
+    int a,b=0;
+    printf("출력할 구구단의 범위를 입력하세요!\n: ");
+    scanf("%d %d",&a,&b);
+    if(a>b)
+    {
+        int t=0;
+        t=b;
+        b=a;
+        a=t;
+    }
+    for(int o=a;o<=b;o++)
+    {
+        multiplication_table(o);
+        printf("\n");
+    }
+}
