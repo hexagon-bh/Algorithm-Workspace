e117c6cfa94eab186605f499878e548f42d7bfa3
hyun0810d <hyun0810d@gmail.com>
2021-11-20 17:23:35 +0900

    방향키 조절

---
+++ C/테트리스/방향키_조절.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <windows.h>
+#include <stdlib.h>
+void gotoxy(int x,int y);
+int main(){
+int x=1,y=1;
+gotoxy(x,y);
+while(1){
+if(GetAsyncKeyState(VK_UP)){
+y--;
+Sleep(0.3);
+}
+if(GetAsyncKeyState(VK_DOWN)){
+y++;
+Sleep(0.3);
+}
+if(GetAsyncKeyState(VK_LEFT)){
+x--;
+Sleep(0.3);
+}
+if(GetAsyncKeyState(VK_RIGHT)){
+x++;
+Sleep(0.3);
+}
+system("cls");
+gotoxy(x,y);
+printf("*");
+Sleep(1);
+}
+}
+
+void gotoxy(int x,int y){
+COORD Pos = { x - 1, y - 1 };
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), Pos);
+}
