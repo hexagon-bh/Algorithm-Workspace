404147d004b66dd217f416d02a1de756585cb484
hyun0810d <hyun0810d@gmail.com>
2021-11-21 17:54:57 +0900

    save

--- C/테트리스/draw_map.c
+++ C/테트리스/draw_map.c
@@ -5,6 +5,10 @@ void gotoxy(int x,int y){
 COORD pos={x,y};
 SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
 }
+int draw_map();
+int main(){
+	draw_map();
+}
 int draw_map(){
     int board[21][12] = {
 	{1,0,0,0,0,0,0,0,0,0,0,1},
@@ -28,7 +32,7 @@ int draw_map(){
 	{1,0,0,0,0,0,0,0,0,0,0,1},
 	{1,0,0,0,0,0,0,0,0,0,0,1},
 	{1,1,1,1,1,1,1,1,1,1,1,1}
-};
+	};
     system("cls");
     printf("<TETRIS>\n");
     gotoxy(5,10);
@@ -38,9 +42,54 @@ int draw_map(){
                 printf(" ▦");
             }
             else{
+				if(j!=3){
                 printf("  ");
+				} else{
+					printf("--");
+				}
             }
         }
         gotoxy(5,j+3);
     }
+	gotoxy(27,5);
+	printf("-");
+	//미리보기 미니맵
+	gotoxy(33,3);
+	printf("+-  N E X T  -+ ");
+	gotoxy(33,4);
+	printf("|             | ");
+	gotoxy(33,5);
+	printf("|             | ");
+	gotoxy(33,6);
+	printf("|             | ");
+	gotoxy(33,7);
+	printf("|             | ");
+	gotoxy(33,8);
+	printf("+-- -  -  - --+ ");
+	gotoxy(33,10);
+	printf("LEVEL: ");
+	gotoxy(33,11);
+	printf("GOAL: ");
+	gotoxy(33,13);
+	printf("YOUR SCORE: ");
+	gotoxy(33,14);
+	printf("LAST SCORE: ");
+	gotoxy(33,15);
+	printf("BEST SCORE: ");
+    //방향키 설명
+    gotoxy(33,17);
+    printf("  ▲   : UP");
+    gotoxy(33,18);
+    printf("◀   ▶ : Left/Right");
+    gotoxy(33,19);
+    printf("  ▼   : DOWN");
+    gotoxy(33,20);
+    printf("  P   : DOWN");
+    gotoxy(33,21);
+    printf("  Q   : QUIT");
+	//command
+	gotoxy(6,24);
+	printf(">>");
+	//전체 보기
+	gotoxy(5,25);
 }
--- C/테트리스/tetris.c
+++ C/테트리스/tetris.c
@@ -42,7 +42,7 @@ COORD Pos = { x - 1, y - 1 };
 }
 //draw_map
 int draw_map(){
-    system("cls");
+ system("cls");
     printf("<TETRIS>\n");
     gotoxy(5,10);
     for(int j=0;j<=20;j++){
@@ -51,11 +51,56 @@ int draw_map(){
                 printf(" ▦");
             }
             else{
+				if(j!=3){
                 printf("  ");
+				} else{
+					printf("--");
+				}
             }
         }
         gotoxy(5,j+3);
     }
+	gotoxy(27,5);
+	printf("-");
+	//미리보기 미니맵
+	gotoxy(33,3);
+	printf("+-  N E X T  -+ ");
+	gotoxy(33,4);
+	printf("|             | ");
+	gotoxy(33,5);
+	printf("|             | ");
+	gotoxy(33,6);
+	printf("|             | ");
+	gotoxy(33,7);
+	printf("|             | ");
+	gotoxy(33,8);
+	printf("+-- -  -  - --+ ");
+	gotoxy(33,10);
+	printf("LEVEL: ");
+	gotoxy(33,11);
+	printf("GOAL: ");
+	gotoxy(33,13);
+	printf("YOUR SCORE: ");
+	gotoxy(33,14);
+	printf("LAST SCORE: ");
+	gotoxy(33,15);
+	printf("BEST SCORE: ");
+    //방향키 설명
+    gotoxy(33,17);
+    printf("  ▲   : UP");
+    gotoxy(33,18);
+    printf("◀   ▶ : Left/Right");
+    gotoxy(33,19);
+    printf("  ▼   : DOWN");
+    gotoxy(33,20);
+    printf("  P   : PAUSE");
+    gotoxy(33,21);
+    printf("  Q   : QUIT");
+	//command
+	gotoxy(6,24);
+	printf(">>");
+	//전체 보기
+	gotoxy(5,25);
 }
 //menu
 int menu(){
@@ -112,7 +157,7 @@ int home(){
     gotoxy(5,12);
     printf("  ▼   : DOWN");
     gotoxy(5,13);
-    printf("  P   : DOWN");
+    printf("  P   : PAUSE");
     gotoxy(5,14);
     printf("  Q   : QUIT");
     //command
