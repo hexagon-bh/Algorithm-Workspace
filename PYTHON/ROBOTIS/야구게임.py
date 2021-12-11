c587a18ebefe776d69080887dae17a8a23013a9e
hyun0810d <hyun0810d@gmail.com>
2021-12-11 14:38:38 +0900

    야구게임

--- PYTHON/ROBOTIS/야구게임.py
+++ PYTHON/ROBOTIS/야구게임.py
@@ -3,16 +3,19 @@ import sys
 pc=[0,0,0,0]
 user=[0,0,0,0]
 strike=0
-pc[0]=int(random()*10)+1
-pc[1]=int(random()*10)+1
-pc[2]=int(random()*10)+1
-pc[3]=int(random()*10)+1
-for i in range(3):
+pc[0]=int(random()*9)+1
+pc[1]=int(random()*9)+1
+pc[2]=int(random()*9)+1
+pc[3]=int(random()*9)+1
+print(pc)
+for i in range(4):
     user[i]=int(input())
     if user[i]>9:
         sys.exit()
-for j in range(3):
+for j in range(4):
     if pc[j]==user[j]:
         strike+=1
-if strike==4:
-
+if strike<4:
+    print(strike,"스타라이크")
+elif strike==4:
+    print("홈런")
