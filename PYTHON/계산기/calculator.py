2d46b5c5bcd4418398859151d22aac3ef52cc105
hyun0810d <hyun0810d@gmail.com>
2021-12-14 12:20:24 +0900

    d

--- PYTHON/계산기/calculator.py
+++ PYTHON/계산기/calculator.py
@@ -9,8 +9,13 @@ pr.pack()
 
 def prbt1():
     pr.config(text="1")
+def prbt2():
+    pr.config(text="2")
 
 number1=Button(root,padx=20,pady=15,fg="black",bg="white",relief="groove",borderwidth=1, text='1',command=prbt1)
 number1.pack()
+number2=Button(root,padx=20,pady=15,fg="black",bg="white",relief="groove",borderwidth=1, text='2',command=prbt2)
+number2.pack()
+
 
 root.mainloop()
