faabffdeef831ba0a967954557dbc1f447faa992
hyun0810d <84117112+hyun0810d@users.noreply.github.com>
2021-12-16 22:29:34 +0900

    save

--- PYTHON/유튜브 분석기/youtbe_crawling_practice.py
+++ PYTHON/유튜브 분석기/youtbe_crawling_practice.py
@@ -5,16 +5,14 @@ options = webdriver.ChromeOptions()
 options.add_experimental_option("excludeSwitches", ["enable-logging"])
 browser = webdriver.Chrome(options=options)
 browser = webdriver.Chrome()
-
-browser.get("https://www.youtube.com/c/tvddotty/videos")
+# title
+browser.get("https://www.youtube.com/c/%EC%8A%B9%EC%9A%B0%EC%95%84%EB%B9%A0/videos")
 html = browser.page_source
 soup = BeautifulSoup(html, 'html.parser')
-# title
 title=soup.find_all("a",attrs={"class":"yt-simple-endpoint style-scope ytd-grid-video-renderer"})
-for n in title:
-    print(n.text.strip())
+for n in title: print(n.text.strip())
 #구독자
-browser.get("https://www.youtube.com/c/tvddotty/featured")
+browser.get("https://www.youtube.com/c/%EC%8A%B9%EC%9A%B0%EC%95%84%EB%B9%A0/featured")
 html = browser.page_source
 soup = BeautifulSoup(html, 'html.parser')
 subscribe=soup.find_all("yt-formatted-string",attrs={"id":"subscriber-count"})
