d27668b380d607a42ae1b8391b33719cfd4d0ebd
hyun0810d <hyun0810d@gmail.com>
2022-04-04 01:05:49 +0900

    Youtube Analyzer

--- PYTHON/ROBOTIS/후위연산자2.py
+++ PYTHON/ROBOTIS/후위연산자2.py
@@ -1,9 +1,28 @@
-h_form=input()
-h_form=list(h_form)
-while True:
-    try:
-        int(h_form)
-    except:
-        pass
-    else:
-        
+from multiprocessing.reduction import duplicate
+value=str(input())
+list=value.split()
+ma=len(list)
+print(ma)
+print(list)
+#중복제거
+dupli_list=[]
+a=[]
+for v in list:
+    if v in list and v!="+" and v!="-" and v!="*" and v!="/" :
+        a.append(str(v))
+for v in a:
+    if v not in dupli_list:
+        dupli_list.append(v)
+print(dupli_list)
+#요소 개수
+max=len(dupli_list)
+#부호 빼기
+list_number=[]
+for i in range(0,max):
+    value=int(input(dupli_list[i]+": "))
+    list_number.append(value)
+for j in range(0,max):
+    for o in range(0,ma):
+        if list[o]==dupli_list[j]:
+            list[o]=list_number[j]
+print(list)
