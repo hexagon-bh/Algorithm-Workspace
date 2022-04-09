792b5531790b88bfb24ed3b63867a100de6271c5
hyun0810d <hyun0810d@gmail.com>
2022-04-09 15:48:58 +0900

    후위연산자 완료!!!!!!!!!!!

--- PYTHON/ROBOTIS/후위연산자2.py
+++ PYTHON/ROBOTIS/후위연산자2.py
@@ -1,9 +1,8 @@
+from hashlib import pbkdf2_hmac
 from multiprocessing.reduction import duplicate
 value=str(input())
 list=value.split()
 ma=len(list)
-print(ma)
-print(list)
 #중복제거
 dupli_list=[]
 a=[]
@@ -13,7 +12,6 @@ for v in list:
 for v in a:
     if v not in dupli_list:
         dupli_list.append(v)
-print(dupli_list)
 #요소 개수
 max=len(dupli_list)
 #부호 빼기
@@ -25,4 +23,43 @@ for j in range(0,max):
     for o in range(0,ma):
         if list[o]==dupli_list[j]:
             list[o]=list_number[j]
+while True:
+    p=0
+    for p in  range(0,ma):
+        try: 
+            ok=list[p].isalpha()
+        except:
+            pass
+        else:
+            break
+    if list[p]=="+":
+        result=int(list[p-2])+int(list[p-1])
+        list[p]=result
+        del  list[p-1]
+        del list[p-2]
+        ma=len(list)
+        continue
+    if list[p]=="-":
+        result=int(list[p-2])-int(list[p-1])
+        list[p]=result
+        del  list[p-1]
+        del list[p-2]
+        ma=len(list)
+        continue
+    if list[p]=="*":
+        result=int(list[p-2])*int(list[p-1])
+        list[p]=result
+        del  list[p-1]
+        del list[p-2]
+        ma=len(list)
+        continue
+    if list[p]=="/":
+        result=int(list[p-2])/int(list[p-1])
+        list[p]=result
+        del  list[p-1]
+        del list[p-2]
+        ma=len(list)
+        continue
+    if ma==1:
+        break
 print(list)
--- PYTHON/SRC/Youtube_Analyze/video_info.py
+++ PYTHON/SRC/Youtube_Analyze/video_info.py
@@ -5,17 +5,17 @@ from selenium.webdriver.common.keys import Keys
 import time
 from pynput.mouse import Button, Controller
 
-url="https://www.youtube.com/c/Owlsreview/videos"
+url="https://www.youtube.com/c/NormalticPlace/videos"
 options = webdriver.ChromeOptions()
-options.add_argument("headless")
-driver = webdriver.Chrome(executable_path="C:\code\WORKSPACE/PYTHON/SRC/유튜브 분석기/chromedriver.exe",chrome_options=options)
+# options.add_argument("headless")
+driver = webdriver.Chrome(executable_path="C:\code\WORKSPACE/PYTHON/SRC/Youtube_Analyze/chromedriver.exe",chrome_options=options)
 driver.get(url=url)
 driver.maximize_window()
 mouse = Controller()
 while (1):
     before_location = driver.execute_script("return window.pageYOffset")
-    driver.execute_script("window.scrollTo(0,{})".format(before_location + 100000))
-    time.sleep(1) 
+    driver.execute_script("window.scrollTo(0,{})".format(before_location + 100))
+    time.sleep(0.3)
     after_location = driver.execute_script("return window.pageYOffset") 
     if before_location == after_location: 
         mouse.position = (1872, 1005)
