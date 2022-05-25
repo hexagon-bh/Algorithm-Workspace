e122d44ce0b24901887582669c4626f1b67f929c
hyun0810d <84117112+hyun0810d@users.noreply.github.com>
2022-05-25 20:33:18 +0900

    중간 저장!!!!!!!이거 날리면 안돼ㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠ

--- school/encoder.ino
+++ school/encoder.ino
@@ -1,12 +1,18 @@
 #define CLK 12    // 2번핀을 CLK로 지정
 #define DT 11    // 3번핀을 DT로 지정
 #define SW 10     // 4번핀을 스위치핀으로 지정
-const int speaker = 8;
+#include <RH_ASK.h>
+#include <SPI.h> // Not actually used but needed to compile
+#include <time.h>
+
+RH_ASK driver;
+
+char buff[256];
+int value = 33;
 int counter = 0;           // 회전 카운터 측정용 변수
 int currentStateCLK;       // CLK의 현재 신호상태 저장용 변수
 int lastStateCLK;          // 직전 CLK의 신호상태 저장용 변수 
-String currentDir ="";      // 현재 회전 방향 출력용 문자 
-//열 저장 변수
+String currentDir ="";      // 현재 회전 방향 출력용 문자열 저장 변수
 unsigned long lastButtonPress = 0;     // 버튼 눌림 상태 확인용 변수
 
 void setup() {
@@ -15,7 +21,7 @@ void setup() {
   pinMode(CLK,INPUT);
   pinMode(DT,INPUT);
   pinMode(SW, INPUT_PULLUP);     // 스위치핀은 내부 풀업저항 사용
-  pinMode (speaker,OUTPUT);
+
   // 시리얼통신 개시
   Serial.begin(9600);
 
@@ -34,28 +40,14 @@ void loop() {
     // DT핀의 신호를 확인해서 엔코더의 회전 방향을 확인함.
 
     if (digitalRead(DT) != currentStateCLK) {  // 신호가 다르다면 시계방향 회전
-      if (counter==11){
-        counter=11;
-        tone(speaker,440,500);
-        noTone(8);
-      }
-      else{
       counter ++;                           // 카운팅 용 숫자 1 증가
-      currentDir ="Right rotation";
-      }
+      currentDir ="시계방향 회전";
     } else {                                 // 신호가 같다면 반시계방향 회전
-      if (counter==-11){                                                                           
-        counter=-11;
-        tone(speaker,440,500);
-        noTone(8);
-      }
-      else{
       counter --;                         // 카운팅 용 숫자 1 감소
-      currentDir ="Left rotation";
-      }
+      currentDir ="반시계 방향 회전";
     }
       
-    Serial.print("Rotation Direction: ");             
+    Serial.print("회전방향: ");             
     Serial.print(currentDir);           //회전방향 출력
     Serial.print(" | Counter: ");
     Serial.println(counter);           // 회전 카운팅 출력
@@ -71,13 +63,24 @@ void loop() {
   if (btnState == LOW) {
     //버튼이 눌린지 50ms가 지났는지 확인, 즉 버튼이 한번 눌린 후 최소 50 ms는 지나야 버튼이 다시 눌린것으로 감지
     if (millis() - lastButtonPress > 50) {  // 50ms 이상 지났다면 
-      Serial.println("horn!");  //버튼 눌림 메시지 출력
+      value==100;
+      Serial.println("버튼 눌림!");  //버튼 눌림 메시지 출력
     }
 
     // 마자막 버튼이 눌린 시간 저장
     lastButtonPress = millis();
   }
-
+    if (value==101){
+     sprintf(buff,"%d",value); //int to char
+     Serial.println(buff);
+     const char *msg = buff;
+     if (driver.send((uint8_t *)msg, strlen(msg))) {
+         driver.waitPacketSent();
+         Serial.println(msg);
+      }
+     delay(300);
+     
   // 잠시 대기
   delay(1);
-}
+    }
+    }
