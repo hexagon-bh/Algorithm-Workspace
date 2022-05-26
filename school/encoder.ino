a5f329c70e526bd653b9fdb696c3b831a994aad2
hyun0810d <84117112+hyun0810d@users.noreply.github.com>
2022-05-26 20:00:46 +0900

    Update encoder.ino

--- school/encoder.ino
+++ school/encoder.ino
@@ -8,12 +8,13 @@
 RH_ASK driver;
 
 char buff[256];
-int value = 33;
+int value = 300;
 int counter = 0;           // 회전 카운터 측정용 변수
 int currentStateCLK;       // CLK의 현재 신호상태 저장용 변수
 int lastStateCLK;          // 직전 CLK의 신호상태 저장용 변수 
 String currentDir ="";      // 현재 회전 방향 출력용 문자열 저장 변수
 unsigned long lastButtonPress = 0;     // 버튼 눌림 상태 확인용 변수
+int a=0;
 
 void setup() {
   
@@ -21,6 +22,8 @@ void setup() {
   pinMode(CLK,INPUT);
   pinMode(DT,INPUT);
   pinMode(SW, INPUT_PULLUP);     // 스위치핀은 내부 풀업저항 사용
+  pinMode(2,OUTPUT);
+  pinMode(6,INPUT_PULLUP);
 
   // 시리얼통신 개시
   Serial.begin(9600);
@@ -30,34 +33,40 @@ void setup() {
 }
 
 void loop() {
-  
+  value==300;
   // CLK핀의 상태를 확인
   currentStateCLK = digitalRead(CLK);
 
   // CLK핀의 신호가 바뀌었고(즉, 로터리엔코더의 회전이 발생했했고), 그 상태가 HIGH이면(최소 회전단위의 회전이 발생했다면) 
   if (currentStateCLK != lastStateCLK  && currentStateCLK == 1){
-
-    // DT핀의 신호를 확인해서 엔코더의 회전 방향을 확인함.
-
+    
+    // DT핀의 신호를 확인해서 엔코더의 회전 방향을 확인함
     if (digitalRead(DT) != currentStateCLK) {  // 신호가 다르다면 시계방향 회전
-      if (counter==11){
-        counter==11
-        value=101;
-      }else{
-      counter ++;// 카운팅 용 숫자 1 증가
-      
-      value=counter;
+      digitalWrite(2,LOW);
+      counter++;// 카운팅 용 숫자 1 증가
       currentDir ="right rotate";
+      //value=coutner;
+      if (counter==12){
+        counter=11;
+        digitalWrite(2,HIGH);
+        value=300;
       }
-    } else {        
-      if (counter==-11){
-        counter==-11
-        value=101;
-      }else{
-      counter --;// 카운팅 용 숫자 1 감소
-      value=counter;
+      else{
+        Serial.print("");
+      }
+      }
+    else   {                                 // 신호가 같다면 반시계방향 회전
+      digitalWrite(2,LOW);
+      counter--;                         // 카운팅 용 숫자 1 감소
       currentDir ="left rotate";
-      }
+     if(counter==-12){
+      digitalWrite(2,HIGH);
+      counter=-11;
+      value=300;
+     }
+     else{
+      Serial.print("");
+     }
     }
       
     Serial.print("roatation director: ");             
@@ -65,26 +74,18 @@ void loop() {
     Serial.print(" | Counter: ");
     Serial.println(counter);           // 회전 카운팅 출력
   }
-
+    
+  if (digitalRead(6)==HIGH){
+    value=260;
+  }
   // 현재의 CLK상태를 저장
   lastStateCLK = currentStateCLK;
 
   // 버튼(스위치)이 눌렸는지 확인
   int btnState = digitalRead(SW);
 
-  // 버튼(스위치)가 눌리면
-  if (btnState == LOW) {
-    //버튼이 눌린지 50ms가 지났는지 확인, 즉 버튼이 한번 눌린 후 최소 50 ms는 지나야 버튼이 다시 눌린것으로 감지
-    if (millis() - lastButtonPress > 50) {  // 50ms 이상 지났다면 
-      value==100;
-      Serial.println("horn!");  //버튼 눌림 메시지 출력
-      value++;
-    }
 
-    // 마자막 버튼이 눌린 시간 저장
-    lastButtonPress = millis();
-  }
-    if (value!=101){
+    if (value!=300){
      sprintf(buff,"%d",value); //int to char
      Serial.println(buff);
      const char *msg = buff;
@@ -93,7 +94,7 @@ void loop() {
          Serial.println(msg);
          Serial.println("It was sent as an RC car.");
       }
-     delay(300);
+
      
   // 잠시 대기
   delay(1);
