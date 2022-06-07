074ba4884e688b2acde36d11d645c80a98dc88f9
hyun0810d <84117112+hyun0810d@users.noreply.github.com>
2022-06-07 20:43:53 +0900

    save: shoes_cleaner.ino & main.py

--- school/main.py
+++ school/main.py
@@ -4,8 +4,11 @@ from bs4 import BeautifulSoup
 import time
 import tkinter
 from datetime import datetime
+import serial
+import time
 four_hour=[]
 def crawling():
+    print("날씨 크롤링 시작")
     rain=["약한비","강한비","비","눈","약한눈","강한눈","진눈깨비","소나기","소낙눈","번개, 뇌우","우박","비 또는 눈","가끔 비","가끔 눈","가끔 비 또는 눈","흐려져 비","흐려져 비(밤)","흐려져 눈","흐려져 눈(밤)"]
     url="https://search.naver.com/search.naver?query=%EA%B0%95%EC%88%98#"
     # options = webdriver.ChromeOptions()
@@ -35,6 +38,7 @@ def crawling():
         four_hour.append(status)
         del four_hour[0]
 def main():
+    print("프로그램 시작")
     old_hour=0
     while True:
         today=datetime.now()
@@ -43,6 +47,7 @@ def main():
             crawling()
         elif old_hour==24 and current_hour==1:
             crawling()
+        old_hour=current_hour
 window=tkinter.Tk()
 window.title("신발 청소기 관리 프로그램")
 title=tkinter.Label(text="블루트스 연결")
