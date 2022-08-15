64225e977198c66499148f431d640ee62416a1ab
hyun0810d <hyun0810d@gmail.com>
2022-08-15 23:24:47 +0900

    맞춤법 검사기.py

---
+++ PYTHON/SRC/맞춤법_검사기.py
@@ -0,0 +1,10 @@
+import tkinter
+import requests
+from selenium import webdriver
+from bs4 import BeautifulSoup
+from selenium.webdriver.common.keys import Keys
+import time
+# options.add_argument("headless")
+driver = webdriver.Chrome(executable_path="C:\code\WORKSPACE/PYTHON/SRC/Youtube_Analyze/chromedriver.exe",chrome_options=options)
+driver.get(url=url)
+
