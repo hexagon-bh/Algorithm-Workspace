6bad08ae58704cc61d23d04ea7f9bd4eddf7aa8e
rkd_gus08 <84117112+hyun0810d@users.noreply.github.com>
2023-06-15 11:47:11 +0900

    save

--- school/대회/2023 코드페어/main.py
+++ school/대회/2023 코드페어/main.py
@@ -16,15 +16,12 @@ def speak(text, lang="ko",speed=False):
 r = speech_recognition.Recognizer()
 data=""
 while True:
-    try:
+    try: #인식&처리에서 에러가 날겨우를 대비한 예외처리
         with speech_recognition.Microphone() as source:
-            print("mic")
-            r.adjust_for_ambient_noise(source)
-            audio=r.listen(source)
-            data=r.recognize_google(audio,language="ko")
-            print("success recog")
+            r.adjust_for_ambient_noise(source) #노이즈가 있는 데이터를 인식
+            audio=r.listen(source) #데이터를 읽음
+            data=r.recognize_google(audio,language="ko") #음성데이터를 문자로 변환함(한국어)
             print(data)
     except:
         r=speech_recognition.Recognizer()
-    if data=="엘리스":
-        speak("안녕하세요")
+    # if data=="엘리스":
