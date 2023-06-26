ae245f0bcd800440dfca644b31e8c68fa7fc7c2a
rkd_gus08 <84117112+hyun0810d@users.noreply.github.com>
2023-06-26 13:39:52 +0900

    RCcar.ino

--- RCcar.ino
+++ RCcar.ino
@@ -1,10 +1,10 @@
 #include <SoftwareSerial.h>
 #include <Servo.h>
 #include <String.h>
-#define BT_RXD 3
-#define BT_TXD 2
+#define BT_RXD 2
+#define BT_TXD 3
 SoftwareSerial hc06(BT_RXD, BT_TXD);
-int value[4]={0,0,0,0};
+int value[4]={0,0,1,0};
 char value_input[10]="0";
 int v_recog=0;
 int t=0;
@@ -29,32 +29,41 @@ void setup(){
   pinMode(dc2_1,OUTPUT);
   pinMode(dc2_2,OUTPUT);
   analogWrite(dc2_s,200);
+  pinMode(speaker,OUTPUT);
 }
  
 void loop(){
-  if (hc06.available()){
+ if (hc06.available()){
     char data=hc06.read();
-    if (data=='*'){
-      Serial.print("        ");
-      Serial.print(value[0]);
-      Serial.print(",");
-      Serial.print(value[1]);
-      Serial.println("");\
+    if (data=='*'){   //Y축(A1)
+      Serial.println("");
       Serial.print("A1(Y축):");
       Serial.print(" ");
       v_recog=0;
       value[v_recog]=0;
     }
-    else if (data=='/'){
+    else if (data=='/'){  //X축(A0)
       Serial.print("        ");
       Serial.print(value[0]);
       Serial.print(",");
       Serial.print(value[1]);
+      Serial.print("  ");
       Serial.println("");
       Serial.print("A0(X축):");
       Serial.print(" ");
       v_recog=1;
       value[v_recog]=0;
+    }
+    else if (data=='-'){  //클락션(D8)
+      Serial.print("        ");
+      Serial.print(value[0]);
+      Serial.print(",");
+      Serial.print(value[1]);
+      Serial.println("");
+      Serial.print("D8(클락션):");
+      Serial.print(" ");
+      v_recog=2;
+      value[v_recog]=1;
     }
     else{
       t=(int)data-48;
@@ -62,15 +71,46 @@ void loop(){
       Serial.print(data);
     }
   }
-  if(value[0]<540){
+    //DC 모터
+    if (value[0]<300){          //1단 후진
     digitalWrite(dc1_1,LOW);
-    digitalWrite(dc1_2,LOW);
+    digitalWrite(dc1_2,HIGH);
+    analogWrite(dc1_s,180);
+    digitalWrite(dc2_1,LOW);
+    digitalWrite(dc2_2,HIGH);
+    analogWrite(dc2_s,180); 
+  }
+    if (value[0]<200){          //2단 후진
+    digitalWrite(dc1_1,LOW);
+    digitalWrite(dc1_2,HIGH);
+    analogWrite(dc1_s,190);
+    digitalWrite(dc2_1,LOW);
+    digitalWrite(dc2_2,HIGH);
+    analogWrite(dc2_s,190); 
+  }
+    if (value[0]<100){          //3단 후진
+    digitalWrite(dc1_1,LOW);
+    digitalWrite(dc1_2,HIGH);
     analogWrite(dc1_s,200);
     digitalWrite(dc2_1,LOW);
-    digitalWrite(dc2_2,LOW);
-    analogWrite(dc2_s,200); 
+    digitalWrite(dc2_2,HIGH);
+    analogWrite(dc2_s,250); 
   }
-  if (value[0]>540){
+  if(value[0]<500 && value[0]<600){     //정지
+    digitalWrite(dc1_1,LOW);
+    digitalWrite(dc1_2,LOW);
+    digitalWrite(dc2_1,LOW);
+    digitalWrite(dc2_2,LOW);
+  }
+  if (value[0]>600){                //1단 전진
+    digitalWrite(dc1_1,HIGH);
+    digitalWrite(dc1_2,LOW);
+    analogWrite(dc1_s,180);
+    digitalWrite(dc2_1,HIGH);
+    digitalWrite(dc2_2,LOW);
+    analogWrite(dc2_s,180); 
+  }
+    if (value[0]>700){              //2단 전진
     digitalWrite(dc1_1,HIGH);
     digitalWrite(dc1_2,LOW);
     analogWrite(dc1_s,200);
@@ -78,15 +118,7 @@ void loop(){
     digitalWrite(dc2_2,LOW);
     analogWrite(dc2_s,200); 
   }
-    if (value[0]>640){
-    digitalWrite(dc1_1,HIGH);
-    digitalWrite(dc1_2,LOW);
-    analogWrite(dc1_s,210);
-    digitalWrite(dc2_1,HIGH);
-    digitalWrite(dc2_2,LOW);
-    analogWrite(dc2_s,210); 
-  }
-    if (value[0]>740){
+    if (value[0]>800){            //3단 전진
     digitalWrite(dc1_1,HIGH);
     digitalWrite(dc1_2,LOW);
     analogWrite(dc1_s,220);
@@ -94,7 +126,7 @@ void loop(){
     digitalWrite(dc2_2,LOW);
     analogWrite(dc2_s,220); 
   }
-    if (value[0]>840){
+    if (value[0]>900){            //4단 전진
     digitalWrite(dc1_1,HIGH);
     digitalWrite(dc1_2,LOW);
     analogWrite(dc1_s,230);
@@ -102,21 +134,29 @@ void loop(){
     digitalWrite(dc2_2,LOW);
     analogWrite(dc2_s,230); 
   }
-  if (value[0]>940){
+  if (value[0]>950){            //5단 전진
     digitalWrite(dc1_1,HIGH);
     digitalWrite(dc1_2,LOW);
-    analogWrite(dc1_s,230);
+    analogWrite(dc1_s,240);
     digitalWrite(dc2_1,HIGH);
     digitalWrite(dc2_2,LOW);
-    analogWrite(dc2_s,230); 
+    analogWrite(dc2_s,240); 
   }
-  if (value[0]>1000){
+  if (value[0]>1000){           //6단 전진
     digitalWrite(dc1_1,HIGH);
     digitalWrite(dc1_2,LOW);
     analogWrite(dc1_s,255);
     digitalWrite(dc2_1,HIGH);
     digitalWrite(dc2_2,LOW);
     analogWrite(dc2_s,255);
+  }
+  //서보모터
+  //클락션
+  if (value[2]==10){
+    tone(speaker,150);
+  }
+  if (value[2]==11){
+    noTone(speaker);
   }
 
 }
